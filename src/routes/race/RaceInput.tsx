6c2b886738f602b015c1e9d7620eace02e9fdaa9
Aloun Vangkeosay <alounv@gmail.com>
2025-03-15 23:07:13 +0100

    Fix apostroph

--- src/routes/race/RaceInput.tsx
+++ src/routes/race/RaceInput.tsx
@@ -67,7 +67,7 @@ export function RaceInput(props: {
     return props.playerRaces.find((r) => r.playerID === props.z.userID);
   }
   function text() {
-    return props.quote.body.replace(/'/g, "'");
+    return props.quote.body.replace(/’/g, "'");
   }
   function wordIndex() {
     const soFar = text().slice(0, charIndex());
